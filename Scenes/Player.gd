cfbecdc9a3a8e34badf108419645e0ed11d88799
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-04-01 12:23:21 +1300

    character

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -2,6 +2,13 @@ extends KinematicBody2D
 
 export (int) var speed = 120
 
+var velocity = Vector2.ZERO
+
+export (float) var acceleration = 25
+
+enum state {IDLE, STRAIGHT, EXPLOSION, LEFT, RIGHT}
+
+
 func _ready():
 	$AnimationPlayer.play("Straight")
 	pass 
