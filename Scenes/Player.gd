613cf3d1337e57acb4ae07595924498e1b33ad6c
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-04-04 13:23:34 +1200

    code

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -8,6 +8,8 @@ export (float) var acceleration = 25
 
 enum state {IDLE, STRAIGHT, EXPLOSION, LEFT, RIGHT}
 
+onready var player_state = state.STRAIGHT
+
 
 func _ready():
 	$AnimationPlayer.play("Straight")
