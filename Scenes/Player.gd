38e0925d0ea16b348e5dcb6ab6db1f3496f8218c
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-05-16 14:56:36 +1200

    movement

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -18,3 +18,22 @@ func _ready():
 func update_animation(anim):
 	$AnimationPlayer.play(anim)
 	
+func handle_state(state):
+	print(state)
+	pass
+	
+func get_input():
+	var dir = Input.get_action_strength("right") - Input.get_action_strength("left")
+	if dir != 0:
+		velocity.x = move_toward(velocity.x, dir*speed, acceleration)
+		
+func _physics_process(delta):
+	get_input()
+	if velocity == Vector2.ZERO:
+		player_state = state.IDLE
+	elif velocity.x != 0:
+		player_state = state.STRAIGHT
+		
+	handle_state(player_state)
+	update_animation(player_state)	
+	
