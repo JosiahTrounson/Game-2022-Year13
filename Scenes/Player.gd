72123ff5544b115838aa87cb3c562ee0ab8f7ea7
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-05-18 10:00:21 +1200

    movement

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -16,7 +16,13 @@ func _ready():
 	pass 
 
 func update_animation(anim):
-	$AnimationPlayer.play(anim)
+	match anim:
+		state.IDLE:
+			$AnimationPlayer.play("Straight")
+		state.STRAIGHT:
+			$AnimationPlayer.play("Straight")
+		state.EXPLOSION:
+			$AnimationPlayer.play("Explosion")
 	
 func handle_state(state):
 	print(state)
@@ -26,6 +32,8 @@ func get_input():
 	var dir = Input.get_action_strength("right") - Input.get_action_strength("left")
 	if dir != 0:
 		velocity.x = move_toward(velocity.x, dir*speed, acceleration)
+	else:
+		velocity.x = move_toward(velocity.x, 0,acceleration)
 		
 func _physics_process(delta):
 	get_input()
@@ -33,7 +41,13 @@ func _physics_process(delta):
 		player_state = state.IDLE
 	elif velocity.x != 0:
 		player_state = state.STRAIGHT
+	elif velocity.x != 0:
+		player_state = state.RIGHT
+	elif velocity.x != 0:
+		player_state = state.LEFT
 		
 	handle_state(player_state)
 	update_animation(player_state)	
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
 	
