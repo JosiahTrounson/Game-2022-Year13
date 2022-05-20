950bb218315b4e3519b9cc27ff3a1e23d4481efc
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-05-20 12:04:41 +1200

    movement

--- Scenes/Player.gd
+++ Scenes/Player.gd
@@ -17,10 +17,14 @@ func _ready():
 
 func update_animation(anim):
 	match anim:
-		state.IDLE:
-			$AnimationPlayer.play("Straight")
 		state.STRAIGHT:
 			$AnimationPlayer.play("Straight")
+		state.BACKWARDS:
+			$AnimationPlayer.play("Backwards")
+		state.RIGHT:
+			$AnimationPlayer.play("Right")
+		state.LEFT:
+			$AnimationPlayer.play("Left")
 		state.EXPLOSION:
 			$AnimationPlayer.play("Explosion")
 	
@@ -31,35 +35,58 @@ func handle_state(player_state):
 	pass
 	
 func get_input():
-	var dir = Input.get_action_strength("right") - Input.get_action_strength("left")
-	if dir != 0:
-		velocity.x = move_toward(velocity.x, dir*speed, acceleration)
-	else:
-		velocity.x = move_toward(velocity.x, 0,acceleration)
-	var dir = Input.get_action_strength("up") - Input.get_action_strength("down")
-	if dir != 0:
-		velocity.y = move_toward(velocity.y, dir*speed, acceleration)
-	else:
-		velocity.y = move_toward(velocity.y, 0,acceleration)
-		
+	velocity = Vector2.ZERO
+	if Input.is_action_pressed('right'):
+		velocity.x += 1
+	if Input.is_action_pressed('left'):
+		velocity.x -= 1
+	if Input.is_action_pressed('down'):
+		velocity.y += 1
+	if Input.is_action_pressed('up'):
+		velocity.y -= 1
+	# Make sure diagonal movement isn't faster
+	velocity = velocity.normalized() * speed
+
 func _physics_process(delta):
 	get_input()
+	velocity = move_and_slide(velocity)
 	if velocity == Vector2.ZERO:
-		player_state = state.IDLE
-	elif velocity.x != 0:
 		player_state = state.STRAIGHT
-	elif velocity.x != 0:
+	if Input.is_action_just_pressed("right"):
 		player_state = state.RIGHT
 	elif velocity.x != 0:
-		player_state = state.LEFT
-	elif velocity.y != 0:
 		player_state = state.STRAIGHT
-	elif velocity.y != 0:
-		player_state = state.BACKWARDS
 	
+	#var dir = Input.get_action_strength("right") - Input.get_action_strength("left")
+	#if dir != 0:
+		#velocity.x = move_toward(velocity.x, dir*speed, acceleration)
+	#else:
+		#velocity.x = move_toward(velocity.x, 0,acceleration)
+	#dir = Input.get_action_strength("up") - Input.get_action_strength("down")
+	#if dir != 0:
+		#velocity.y = move_toward(velocity.y, dir*speed, acceleration)
+	#else:
+	#	velocity.y = move_toward(velocity.y, 0, acceleration)
+	#print(velocity)
 		
-	handle_state(player_state)
-	update_animation(player_state)	
-	
-	velocity = move_and_slide(velocity, Vector2.UP)
-	
+#func _physics_process(delta):
+	#get_input()
+	#if velocity == Vector2.ZERO:
+#		player_state = state.IDLE
+#	elif velocity.x != 0:
+#		player_state = state.STRAIGHT
+#	elif velocity.x != 0:
+#		player_state = state.RIGHT
+#	elif velocity.x != 0:
+#		player_state = state.LEFT
+#	elif velocity.y != 0:
+#		player_state = state.STRAIGHT
+#	elif velocity.y != 0:
+#		player_state = state.BACKWARDS
+
+		
+#	handle_state(player_state)
+#	update_animation(player_state)	
+#
+#	velocity = move_and_slide(velocity,)
+#
