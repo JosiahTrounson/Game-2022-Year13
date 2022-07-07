b0bc347cf37f8461b5dec264a496c3a90534af23
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-07-07 14:58:11 +1200

    Enemy Movement

--- Scenes/Curving Enemy.gd
+++ Scenes/Curving Enemy.gd
@@ -2,18 +2,23 @@ extends KinematicBody2D
 
 var direction = Vector2.ZERO
 onready var player = get_tree().get_nodes_in_group("Player")[0]
-var rotation_speed = 1
-var speed = 10
+export var rotation_speed = 10
+export var speed = 100
 var velocity = Vector2.ZERO
-
+export (bool) var random_movement = false
 func _ready():
-	pass
+	
+	direction = (player.global_position - global_position).normalized()
 
+func slow_rotate(rotate_speed):
+	direction = (player.global_position - global_position).normalized()
+	global_rotation = lerp_angle(global_rotation, direction.angle(), rotate_speed )
+	move_and_slide(transform.x * speed,Vector2.ZERO)
+	
+	
 func _process(delta):
-#	var angle_to_plane = get_angle_to(player.global_position)
-#	rotation = move_toward(rotation,angle_to_plane,rotation_speed * delta)
-#	velocity = transform.x * speed
-#	move_and_slide(velocity)
-	translate(Vector2.DOWN * speed * delta)
-	position.x += cos(position.y * 0.5 * delta)
-	position.y += sin(position.x * delta)
+	slow_rotate(rotation_speed * delta)
+
+	if random_movement:
+		position.x += cos(position.y * 0.5 * delta)
+		position.y += sin(position.x * delta)
--- Sprites/Enemy1.gd
+++ Sprites/Enemy1.gd
@@ -10,3 +10,26 @@ func handle_hit():
 
 #func _ready():
 #	pass 
+
+var direction = Vector2.ZERO
+onready var player = get_tree().get_nodes_in_group("Player")[0]
+export var rotation_speed = 10
+export var speed = 10
+var velocity = Vector2.ZERO
+export (bool) var random_movement = false
+func _ready():
+	
+	direction = (player.global_position - global_position).normalized()
+
+func slow_rotate(rotate_speed):
+	direction = (player.global_position - global_position).normalized()
+	global_rotation = lerp_angle(global_rotation, direction.angle(), rotate_speed )
+	move_and_slide(transform.x * speed,Vector2.ZERO)
+	
+	
+func _process(delta):
+	slow_rotate(rotation_speed * delta)
+
+	if random_movement:
+		position.x += cos(position.y * 0.5 * delta)
+		position.y += sin(position.x * delta)
