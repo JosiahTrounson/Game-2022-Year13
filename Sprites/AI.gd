33be12e060ec6ab080508a55f3c68f512c541bdc
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-08-02 10:00:23 +1200

    AI

--- Sprites/AI.gd
+++ Sprites/AI.gd
@@ -1,7 +1,29 @@
 extends Node2D
 
 
-enum {
+signal state_changed(new_state)
+
+
+enum State {
 	PATROL,
 	ENGAGE
 }
+
+
+onready var player_detection_zone = $PlayerDetectionZone
+
+
+var current_state: int = State.PATROL setget set_state
+var player = null
+
+
+func set_state(new_state: int):
+	if new_state == current_state:
+		return
+	
+	current_state = new_state
+	emit_signal("state_changed", current_state)
+
+
+func _on_PlayerDetectionZone_body_entered(body):
+	pass # Replace with function body.
