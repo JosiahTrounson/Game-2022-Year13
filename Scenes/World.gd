841f138b008552158b99c272ccc969ebb4df2ca8
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-09-05 09:44:10 +1200

    Checkpoint

--- Scenes/World.gd
+++ Scenes/World.gd
@@ -4,6 +4,9 @@ extends Node2D
 onready var bullet_manager = $BulletManager
 onready var player = $BulletManager
 
+func _enter_tree():
+	if Checkpoint.LastPosition:
+		$Player.global_position=Checkpoint.LastPosition
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
