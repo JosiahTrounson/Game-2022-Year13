58535d40daee1accd0345b81146b619f5ab55822
JosiahTrounson <jostro67@kingshigh.school.nz>
2022-09-09 14:54:48 +1200

    Reset and cheackpoint

--- Game_stats.gd
+++ Game_stats.gd
@@ -3,10 +3,16 @@ extends Node
 onready var game_start_time = OS.get_ticks_msec()
 var current_spawn = null
 
-func _reset():
+func reset():
 	current_spawn = null
 	get_tree().reload_current_scene()
+	game_start_time = OS.get_ticks_msec()
 	
+func check_reset():
+	if current_spawn == null:
+		reset()
+	else:
+		return false
 
 func set_spawn(spawn):
 	current_spawn = spawn
